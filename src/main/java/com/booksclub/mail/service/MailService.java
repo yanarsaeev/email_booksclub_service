36cc8bb73e3ad7cc67ec4da6c623baaa37995093
yanarsaeev <mackardyback@internet.ru>
2025-10-02 17:12:35 +0300

    create app

---
+++ src/main/java/com/booksclub/mail/service/MailService.java
@@ -0,0 +1,9 @@
+package com.booksclub.mail.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+}
